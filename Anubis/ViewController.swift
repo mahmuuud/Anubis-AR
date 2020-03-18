b5450495e05abd9dabf3029fa4c736d5ce4928a0
Mahmoud Elkassry <mahmoud.elkassry@robustastudio.com>
2020-03-18 18:13:30 +0200

    Initial Commit

---
+++ Anubis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anubis
+//
+//  Created by mahmoud mohamed on 3/18/20.
+//  Copyright © 2020 ASUFE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
